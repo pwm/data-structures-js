f9be1ca145c5b83184ce6ed6d30a332aa31c7490
Zsolt Szende <z.szende@quidco.com>
2016-08-02 17:53:02 +0100

    unifiy min and max start

--- pa2/minHeap.js
+++ pa2/minHeap.js
@@ -1,128 +1,153 @@
-class MinHeap {
-    constructor(getPriorityFn, setPriorityFn) {
-        this.array = [];
-        this.size = 0;
-        this.getPriority = typeof getPriorityFn === 'function'
-            ? getPriorityFn
-            : node => node.priority;
-        this.setPriority = typeof setPriorityFn === 'function'
-            ? setPriorityFn
-            : (node, newPriority) => node.priority = newPriority;
-    }
+'use strict';
 
-    build(a) {
-        this.array = a;
-        this.size = a.length;
-        for (let key = Math.floor((this.size - 1) / 2); key >= 0; key--) {
-            this._siftDown(key);
+const MinHeap = (() => {
+    const TYPE_MAX = 'max';
+    const TYPE_MIN = 'min';
+    const TYPES = [
+        TYPE_MAX,
+        TYPE_MIN
+    ];
+
+    class MinHeap {
+        constructor(type, getPriorityFn, setPriorityFn) {
+            if (! TYPES.includes(type)) {
+                throw Error('Invalid type.');
+            }
+            this.type = type;
+
+            this.array = [];
+            this.size = 0;
+            this.getPriority = typeof getPriorityFn === 'function'
+                ? getPriorityFn
+                : x => x;
+            this.setPriority = typeof setPriorityFn === 'function'
+                ? setPriorityFn
+                : (e, x) => e = x;
+        }
+
+        build(a) {
+            this.array = a;
+            this.size = a.length;
+            for (let key = Math.floor((this.size - 1) / 2); key >= 0; key--) {
+                this._siftDown(key);
+            }
+        }
+
+        insert(node) {
+            this.array[this.size] = node;
+            this._siftUp(this.size);
+            this.size++;
+        }
+
+        getSize() {
+            return this.size;
+        }
+
+        getMin() {
+            return this.array[0];
+        }
+
+        extractMin() {
+            const min = this.array[0];
+            // copy last to its place, this keep the tree complete
+            this.array[0] = this.array[this.size - 1];
+            this.size--;
+            this._siftDown(0); // sift down copied last to its place
+            this.array.pop(); // remove copied last from top
+            return min;
+        }
+
+        changePriority(key, nodeWithNewPriority) {
+            const currentPriority = this.getPriority(this.array[key]);
+            this.array[key] = nodeWithNewPriority;
+            this.getPriority(nodeWithNewPriority) < currentPriority
+                ? this._siftUp(key)
+                : this._siftDown(key);
+        }
+
+        removeNode(key) {
+            this.setPriority(this.array[key], Number.MIN_SAFE_INTEGER);
+            this._siftUp(key);
+            this.extractMin();
+        }
+
+        _siftUp(key) {
+            while (key > 0 && this.getPriority(this.array[MinHeap._getParent(key)]) > this.getPriority(this.array[key])) {
+                MinHeap._swap(this.array, MinHeap._getParent(key), key);
+                key = MinHeap._getParent(key);
+            }
+        }
+
+        _siftDown(key) {
+            let maxKey = key;
+            let leftChild = MinHeap._getLeftChild(key);
+            if (leftChild < this.size && this.getPriority(this.array[leftChild]) < this.getPriority(this.array[maxKey])) {
+                maxKey = leftChild;
+            }
+            let rightChild = MinHeap._getRightChild(key);
+            if (leftChild < this.size && this.getPriority(this.array[rightChild]) < this.getPriority(this.array[maxKey])) {
+                maxKey = rightChild;
+            }
+            if (key !== maxKey) {
+                MinHeap._swap(this.array, key, maxKey);
+                this._siftDown(maxKey);
+            }
+        }
+
+        static _getParent(key) {
+            return Math.floor((key - 1) / 2);
+        }
+
+        static _getLeftChild(key) {
+            return key * 2 + 1;
+        }
+
+        static _getRightChild(key) {
+            return key * 2 + 2;
+        }
+
+        static _swap(a, x, y) {
+            [a[y], a[x]] = [a[x], a[y]];
         }
     }
 
-    insert(node) {
-        this.array[this.size] = node;
-        this._siftUp(this.size);
-        this.size++;
-    }
-    
-    getSize() {
-        return this.size;
-    }
-
-    getMin() {
-        return this.array[0];
-    }
-
-    extractMin() {
-        const min = this.array[0];
-        // copy last to its place, this keep the tree complete
-        this.array[0] = this.array[this.size - 1];
-        this.size--;
-        this._siftDown(0); // sift down copied last to its place
-        this.array.pop(); // remove copied last from top
-        return min;
-    }
-
-    changePriority(key, nodeWithNewPriority) {
-        const currentPriority = this.getPriority(this.array[key]);
-        this.array[key] = nodeWithNewPriority;
-        this.getPriority(nodeWithNewPriority) < currentPriority
-            ? this._siftUp(key)
-            : this._siftDown(key);
-    }
-
-    removeNode(key) {
-        this.setPriority(this.array[key], Number.MIN_SAFE_INTEGER);
-        this._siftUp(key);
-        this.extractMin();
-    }
-
-    _siftUp(key) {
-        while (key > 0 && this.getPriority(this.array[MinHeap._getParent(key)]) > this.getPriority(this.array[key])) {
-            MinHeap._swap(this.array, MinHeap._getParent(key), key);
-            key = MinHeap._getParent(key);
-        }
-    }
-
-    _siftDown(key) {
-        let maxKey = key;
-        let leftChild = MinHeap._getLeftChild(key);
-        if (leftChild < this.size && this.getPriority(this.array[leftChild]) < this.getPriority(this.array[maxKey])) {
-            maxKey = leftChild;
-        }
-        let rightChild = MinHeap._getRightChild(key);
-        if (leftChild < this.size && this.getPriority(this.array[rightChild]) < this.getPriority(this.array[maxKey])) {
-            maxKey = rightChild;
-        }
-        if (key !== maxKey) {
-            MinHeap._swap(this.array, key, maxKey);
-            this._siftDown(maxKey);
-        }
-    }
-
-    static _getParent(key) {
-        return Math.floor((key - 1) / 2);
-    }
-
-    static _getLeftChild(key) {
-        return key * 2 + 1;
-    }
-
-    static _getRightChild(key) {
-        return key * 2 + 2;
-    }
-
-    static _swap(a, x, y) {
-        [a[y], a[x]] = [a[x], a[y]];
-    }
-}
+    return MinHeap;
+})();
 
 ////////////////////////////////
 
-MinHeap.prototype.display = function() {
-    const p = [],
-        d = [];
-    let height = 0;
-    for (let i = 0; i < this.size; i++) {
-        if (i === Math.pow(2, height + 1) - 1) {
-            height++;
-        }
-        if (! Array.isArray(p[height])) {
-            p[height] = []; d[height] = [];
-        }
-        let currPriority = this.getPriority(this.array[i]);
-        p[height].push(currPriority < 10 ? ' ' + currPriority : currPriority);
-        d[height].push(this.array[i].data);
+class DisplayableMinHeap extends MinHeap {
+    constructor(type, getPriorityFn, setPriorityFn) {
+        super(type, getPriorityFn, setPriorityFn);
     }
-    for (let i = 0; i < p.length; i++) {
-        let s = Math.pow(2, p.length - i) - 1;
-        p[i] = ' '.repeat(s) + p[i].join(' '.repeat(s * 2)) + ' '.repeat(s);
-        d[i] = ' '.repeat(s/2) + d[i].join(' '.repeat(s)) + ' '.repeat(s/2);
+
+    display() {
+        const p = [],
+            d = [];
+        let height = 0;
+        for (let i = 0; i < this.size; i++) {
+            if (i === Math.pow(2, height + 1) - 1) {
+                height++;
+            }
+            if (!Array.isArray(p[height])) {
+                p[height] = [];
+                d[height] = [];
+            }
+            let currPriority = this.getPriority(this.array[i]);
+            p[height].push(currPriority < 10 ? ' ' + currPriority : currPriority);
+            d[height].push(this.array[i].data);
+        }
+        for (let i = 0; i < p.length; i++) {
+            let s = Math.pow(2, p.length - i) - 1;
+            p[i] = ' '.repeat(s) + p[i].join(' '.repeat(s * 2)) + ' '.repeat(s);
+            d[i] = ' '.repeat(s / 2) + d[i].join(' '.repeat(s)) + ' '.repeat(s / 2);
+        }
+        p.forEach(priority => console.log(priority));
+        console.log();
+        d.forEach(data => console.log(data));
+        console.log();
     }
-    p.forEach(priority => console.log(priority));
-    console.log();
-    d.forEach(data => console.log(data));
-    console.log();
-};
+}
 
 ////////////////////////////////
 
@@ -135,6 +160,11 @@ class Node {
 
 ////////////////////////////////
 
+//@todo: have a priority-key map to be able to access elements by priority
+//@todo: this however would not work as there can be multiple entries  with the same priority
+//@todo: maybe a map from object to key would work
+
+
 const a = [];
 a.push(new Node(1, 'S'));
 for (let i = 2; i < 63; i++) {
@@ -142,7 +172,8 @@ for (let i = 2; i < 63; i++) {
 }
 a.push(new Node(63, 'E'));
 
-const mh = new MinHeap(
+const mh = new DisplayableMinHeap(
+    'min',
     node => node.priority,
     (node, newPriority) => node.priority = newPriority
 );
